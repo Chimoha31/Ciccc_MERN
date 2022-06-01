2344bed2c3dd082d0e92b92223c541bce7e6edd1
Chimoha31 <mokochii1108@gmail.com>
2022-06-01 01:08:45 -0700

    Styled for cart

--- WeeklyAsignment_May31/client/src/components/CartItem.jsx
+++ WeeklyAsignment_May31/client/src/components/CartItem.jsx
@@ -14,16 +14,16 @@ const CartItem = () => {
       <div className="items">
         <ul>
           <li className="item_details">
-            <p>*Pug T-shirt</p>
             <img
               src="https://cdn.shopify.com/s/files/1/2253/9875/products/new-summer-french-bulldog-printed-t-shirt-women-cute-cartoon-t-shirt-pit-bull-tshirt-top-female-frances-german-shepherd-t-shirt--frenchie-world-shop-30020874207381.jpg?v=1619112230"
               alt="product_item"
             />
+            <p className="item_name">*Pug T-shirt</p>
           </li>
           <li className="item_quantity">
             <div>
-              <h5>-</h5>
-              <p className="quantity">1</p>
+              <h5>−</h5>
+              <input type="number" placeholder="1" className="quantity" />
               <h5>+</h5>
             </div>
           </li>
@@ -32,7 +32,58 @@ const CartItem = () => {
             <span>🗑</span>
           </li>
         </ul>
+        {/* <p>Continue Shopping ←</p> */}
       </div>
+
+      {/* Delete from here */}
+      <div className="items">
+        <ul>
+          <li className="item_details">
+            <img
+              src="https://www.byrdie.com/thmb/22MtiF7yz8Eqv9DJoA4hp7D1VTQ=/1080x810/filters:fill(auto,1)/179765291_593484545376956_4422170819335722433_n-df45c597014d44a7b715330da7e75685.jpg"
+              alt=""
+            />
+            <p>*Foundation</p>
+          </li>
+          <li className="item_quantity">
+            <div>
+              <h5>−</h5>
+              <input type="number" placeholder="1" className="quantity" />
+              <h5>+</h5>
+            </div>
+          </li>
+          <li className="item_total">
+            <p>$55.00</p>
+            <span>🗑</span>
+          </li>
+        </ul>
+      </div>
+      <div className="items">
+        <ul>
+          <li className="item_details">
+            <img
+              src="https://images.ctfassets.net/u4vv676b8z52/4bXMvxB6k8WwyEWSgya9uI/33af104c3b49bcb9cdc465428476586f/Hero-Images-Rihanna.jpg?fm=jpg&q=80"
+              alt="product_item"
+            />
+            <p>*Sanglasses</p>
+          </li>
+          <li className="item_quantity">
+            <div>
+              <h5>−</h5>
+              <input type="number" placeholder="1" className="quantity" />
+              <h5>+</h5>
+            </div>
+          </li>
+          <li className="item_total">
+            <p>$140.00</p>
+            <span>🗑</span>
+          </li>
+        </ul>
+      </div>
+      <div className="continue">
+        <p>←Continue Shopping</p>
+      </div>
+      {/* Delete to here */}
     </div>
   );
 };
