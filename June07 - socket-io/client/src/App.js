b27eb8b2d526c5698ff8f7084441b8b48189cd52
Chimoha31 <mokochii1108@gmail.com>
2022-06-07 16:33:28 -0700

    Coonected with io

--- June07 - socket-io/client/src/App.js
+++ June07 - socket-io/client/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import Chat from "./components/Chat";
 
-function App() {
+const socket = io.connect("http://localhost:2000");
+console.log(socket);
+
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [roomNumber, setRoomNumber] = useState("");
+  const [showChat, setShowchat] = useState(false);
+
+  const enterRoomNumber = (e) => {
+    e.preventDefault();
+
+    if(username !== "" && roomNumber !== "") {
+      // 何をemit()させたいか＝join_room
+      socket.emit("join_room", {username, roomNumber})
+    }else{
+
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="joinChatContainer" onSubmit={enterRoomNumber}>
+        <h1>Join chat</h1>
+        <input
+          type="text"
+          value={username}
+          placeholder="Enter username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="text"
+          value={roomNumber}
+          placeholder="Enter Room Number"
+          onChange={(event) => setRoomNumber(event.target.value)}
+        />
+        <button type="submit">Enter</button>
+      </form>
+      {/* <Chat socket={socket} roomNumber={roomNumber} username={username} />} */}
     </div>
   );
-}
+};
 
 export default App;
--- June07 - socket-io/server/index.js
+++ June07 - socket-io/server/index.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
+var cors = require("cors");
 const PORT = process.env.PORT || 2000;
 
+app.use(express.json());
+app.use(cors());
 
 const io = new Server(server, {
-  conrs: {
+  cors: {
     origin: "http://localhost:3000",
-    methods: ['GET', 'POST']
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log(socket);
+io.on("connection", (socket) => {
+  // サーバーから送信されたメッセージを取得してブラウザの画面に表示する処理 = on (Receive emit() from client)
+  socket.on("join_room", (data) => {
+    console.log(data, "data from client");
+    socket.join(data.roomNumber);
+
+    console.log(`${data.username} connected with ID ${data.id} has joined room: ${data.roomNumber}`)
+  });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running at ${PORT} 🎉`);
-})
+});
