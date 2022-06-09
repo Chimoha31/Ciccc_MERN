83946b66296ae009bc3b8ccea87a07c295eb242e
Chimoha31 <mokochii1108@gmail.com>
2022-06-08 17:49:49 -0700

    Created Server

---
+++ WeeklyAssignment_June10/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import io from 'socket.io-client';
+
+// const socket = io.connect("http://localhost:3001");
+
+const App = () => {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
+
