5a3e655c949455027d99a99ad989cdcdde20f070
Chimoha31 <mokochii1108@gmail.com>
2022-06-09 17:32:29 -0700

    Styling

--- WeeklyAssignment_June10/client/src/App.js
+++ WeeklyAssignment_June10/client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import JoinForm from './components/JoinForm'
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import JoinForm from "./components/JoinForm";
 
 const App = () => {
   return (
-    <div>
-      <JoinForm />
-    </div>
-  )
-}
+    <Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/joinroom" element={<JoinForm />} />
+        </Routes>
+      </Router>
+    </Fragment>
+  );
+};
 
-export default App
+export default App;
---
+++ WeeklyAssignment_June10/client/src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Home.css";
+import { Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <div className="img_container d-flex flex-column align-items-center justify-content-center">
+          <img
+            src="https://left-nav.blogs.hopkinsmedicine.org/files/2016/11/connecting-people.jpg"
+            alt="network"
+            className="home popdown"
+          ></img>
+        </div>
+
+        <div className="popup">
+          <div>
+            <h1>Let's Chat with your friends</h1>
+            <p>This is chat app </p>
+            <LinkContainer to="/joinroom">
+              <Button variant="success">
+                Get Started
+                <i className="fas fa-comments home-message-icon"></i>
+              </Button>
+            </LinkContainer>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
